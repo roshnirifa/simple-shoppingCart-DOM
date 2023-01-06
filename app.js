97c7a615e7191dc85c10469b08e92815f253da1f
roshnirifa <roshnirifa998@gmail.com>
2023-01-06 13:01:32 +0600

    phone case calculation added

--- app.js
+++ app.js
@@ -28,3 +28,33 @@ document.getElementById('phn-minus').addEventListener('click', function () {
 
 
 })
+// common Function for phone case
+function phonecase(isIncreasing) {
+    let phnInput = document.getElementById('case-InputNumber');
+    let phnInputValue = phnInput.value;
+
+    if (isIncreasing) {
+        phnInputValue = parseInt(phnInputValue) + 1;
+    }
+    else if (phnInputValue > 0) {
+        phnInputValue = parseInt(phnInputValue) - 1;
+    }
+    phnInput.value = phnInputValue;
+    // phone total calculation
+    let phoneTotal = document.getElementById('case-total');
+    let phoneTotalValue = phoneTotal.innerText;
+    phoneTotal.innerText = phnInputValue * 40;
+    console.log(phoneTotalValue);
+
+}
+
+
+document.getElementById('phn-case-plus').addEventListener('click', function () {
+    phonecase(true);
+
+})
+document.getElementById('phn-case-minus').addEventListener('click', function () {
+    phonecase(false);
+
+
+})
