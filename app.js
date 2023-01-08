b1d9ec3fa0e17c4cd467d879c46d22a094ad584e
roshnirifa <roshnirifa998@gmail.com>
2023-01-08 21:36:42 +0600

    total calculation added

--- app.js
+++ app.js
@@ -19,7 +19,28 @@ function phone(product, isIncreasing, price) {
 
     productTotal.innerText = productInputValue * price;
     // console.log(phoneTotalValue);
+    calculateTotal();
 }
+
+// function for subtotal
+function getInputValue(product) {
+    let productInput = document.getElementById(product + '-inputNum');
+    let productInputValue = parseInt(productInput.value);
+    return productInputValue;
+}
+function calculateTotal() {
+    let phnTotal = getInputValue("phn") * 1312;
+    let caseTotal = getInputValue("case") * 40;
+    let subTotal = phnTotal + caseTotal;
+    let tax = subTotal / 10;
+    let total = subTotal + tax;
+    document.getElementById('sub-total').innerText = subTotal;
+    document.getElementById('tax-amount').innerText = tax;
+    document.getElementById('total-price').innerText = total;
+}
+
+
+
 document.getElementById('phone-plus').addEventListener('click', function () {
     phone("phn", true, 1312);
 
@@ -42,34 +63,3 @@ document.getElementById('case-minus').addEventListener('click', function () {
 
 
 
-
-// common Function for phone case
-// function phonecase(isIncreasing) {
-//     let caseInput = document.getElementById('case-InputNumber');
-//     let caseInputValue = caseInput.value;
-
-//     if (isIncreasing) {
-//         caseInputValue = parseInt(caseInputValue) + 1;
-//     }
-//     else if (caseInputValue > 0) {
-//         caseInputValue = parseInt(caseInputValue) - 1;
-//     }
-//     caseInput.value = caseInputValue;
-//     // phone total calculation
-//     let caseTotal = document.getElementById('case-total');
-//     let caseTotalValue = caseTotal.innerText;
-//     caseTotal.innerText = caseInputValue * 40;
-//     console.log(caseTotalValue);
-
-// }
-
-
-// document.getElementById('phn-case-plus').addEventListener('click', function () {
-//     phonecase(true);
-
-// })
-// document.getElementById('phn-case-minus').addEventListener('click', function () {
-//     phonecase(false);
-
-
-// })
